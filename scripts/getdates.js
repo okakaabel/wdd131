3ab574fc267f0fe86c2deef1008e3a3576c60bdd
= <=>
2025-01-09 13:55:31 +0300

    work

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the current year in the footer
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Set the last modified date in the footer
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
